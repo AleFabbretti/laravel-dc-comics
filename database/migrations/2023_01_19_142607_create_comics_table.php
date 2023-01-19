e607b34cb445ef3baada6131a4e610036627972a
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-19 16:34:28 +0100

    Aggiunto dati tabella

--- database/migrations/2023_01_19_142607_create_comics_table.php
+++ database/migrations/2023_01_19_142607_create_comics_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
+            $table->string('title', 100);
             $table->string('thumb')->nullable();
             $table->string('series', 50);
             $table->date('sale_date');
-            $table->string('type', 35);
-            $table->string('price', 5);
+            $table->string('type', 30);
+            $table->unsignedFloat('price', 6, 2);
             $table->text('description');
             $table->timestamps();
         });

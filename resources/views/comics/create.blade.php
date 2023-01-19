86c2b071c014cdc4784c2707b724663656fcb2ca
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-19 20:40:28 +0100

    Aggiunto form e store

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('page-content')
+    <h1>Inserisci Nuovo Fumetto</h1>
+    <div class="container">
+        <form action="{{ route('comics.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Thumb</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="series" class="form-label">Serie</label>
+                <input type="text" class="form-control" id="thumb" name="series" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Giorno di uscita</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="type" class="form-label">Tipo</label>
+                <input type="text" class="form-control" id="type" name="type" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="text" class="form-control" id="price" name="price" maxlength="50">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <input type="text" class="form-control" id="description" name="description" maxlength="1000">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Salva</button>
+            <button type="reset" class="btn btn-secondary">Reset</button>
+        </form>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -31,5 +31,6 @@
                 @endforeach
             </tbody>
         </table>
+        <a href="{{ route('comics.create') }}" class="btn btn-secondary">Inserisci nuovo fumetto</a>
     </div>
 @endsection
